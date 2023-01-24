d694e9b1b12290ac505be7870bb5d190c7b831d9
Girish Gopaul <gg@contractbook.dk>
2023-01-24 19:43:06 +0400

    Changed constants

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,3 +1,3 @@
-export const ENDPOINT = 'https://employees-api-elixir.herokuapp.com/v1';
+export const ENDPOINT = 'https://serene-scrubland-83834.herokuapp.com/v1';
 // export const ENDPOINT = 'http://localhost:4000/v1';
-export const PICTURE_BASE_URL = 'https://res.cloudinary.com/employees-api/image/upload/c_thumb,g_face,h_200,w_200'
+export const PICTURE_BASE_URL = 'https://res.cloudinary.com/hk6drdxzs/image/upload/c_thumb,g_face,h_200,w_200'
