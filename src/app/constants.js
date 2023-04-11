6b9f83a265aed3ce43f1be5f8c41ffadf89b52f5
Girish Gopaul <girish@gopaul.me>
2023-04-11 11:43:58 +0400

    Change cloudinary

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,3 +1,3 @@
 // export const ENDPOINT = 'https://mighty-refuge-91305.herokuapp.com';
 export const ENDPOINT = 'https://shielded-escarpment-93823.herokuapp.com/v1';
-export const PICTURE_BASE_URL = 'https://res.cloudinary.com/hqadjhtra/image/upload/c_thumb,g_face,h_200,w_200'
+export const PICTURE_BASE_URL = 'https://res.cloudinary.com/hjyufsewk/image/upload/c_thumb,g_face,h_200,w_200'
